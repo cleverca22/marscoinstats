6226158a9c9dfe48faabea599a3ef3fe735ac40d
michael bishop <cleverca22@gmail.com>
2014-03-01 00:29:30 +0000

    latest changes, average the block time over 10 blocks and predict retarget from that

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	auth:'marscoinrpc:FIXME',
 	nTargetSpacing:123,
-	retarget:721
+	retarget:721,
+	avgperiod:10
 };
--- server.js
+++ server.js
@@ -1,24 +1,76 @@
 var express = require('express');
 var http = require('http');
+var MongoClient = require('mongodb').MongoClient;
 
 var config = require('./config');
-console.log(config);
-
 
 var app = new express();
+var db;
+MongoClient.connect('mongodb://localhost:27017/mars',function (err,dbIN) {
+	db = dbIN;
+	app.listen(8080);
+	console.log('up');
+});
+
+function getblock(hash,cb) {
+	db.collection('blocks').findOne({_id:hash},function (err,row){
+		if (err) console.log(err);
+		if (row) cb(row.block);
+		else {
+			console.log('cache miss',hash);
+			rpc('getblock',[hash],function (reply3) {
+				db.collection('blocks').insert({_id:hash,block:reply3.result},function (err,row){
+					if (err) console.log(err);
+					console.log('cached',hash);
+				});
+				cb(reply3.result);
+			});
+		}
+	});
+}
+var lastcount = 0;
+var cache = null;
+function generateStats(blockindex,cb) {
+	rpc('getblockhash',[blockindex],function (reply2) {
+		var blocks = [];
+		function iter() {
+			var count = config.avgperiod;
+			if (blocks.length > count) {
+				var sum = 0;
+				for (var x=0; x<count; x++) {
+					var a = blocks[x].time;
+					var b = blocks[x+1].time;
+					console.log(x,b-a);
+					sum += b-a;
+				}
+				cache = { avgtime: sum/count, blocks:blocks };
+				console.log(cache,sum,count);
+				lastcount = blockindex;
+				cb();
+			} else {
+				getblock(blocks[0].previousblockhash,function (block) {
+					blocks.unshift(block);
+					iter();
+				});
+			}
+		}
+		getblock(reply2.result,function (block) {
+			blocks.unshift(block);
+			iter();
+		});
+	});
+}
 app.configure(function (){});
 app.set('view engine','jade');
 app.get('/',function (req,res) {
 	rpc('getblockcount',[],function (reply) {
-		rpc('getblockhash',[reply.result],function (reply2) {
-			rpc('getblock',[reply2.result],function (reply3) {
-				rpc('getblock',[reply3.result.previousblockhash],function (reply4) {
-					console.log(reply3.result);
-					console.log(reply4.result);
-					res.render('index',{blocks:reply.result,config:config,lastblock:reply3.result,secondlast:reply4.result});
-				});
-			});
-		});
+		if (reply.result != lastcount) {
+			generateStats(reply.result,finish);
+		} else finish();
+		function finish() {
+			console.log(cache.blocks.length);
+			res.render('index',{config:config,cache:cache,total:lastcount});
+		}
 	});
 });
 function rpc(method,args,cb) {
@@ -36,4 +88,3 @@ function rpc(method,args,cb) {
 	req.write(data);
 	req.end();
 }
-app.listen(8080);
